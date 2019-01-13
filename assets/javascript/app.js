36b8b73e44e885e180bf272d278dbf3449d31177
Christopher Sinlasath <csinlasath@live.com>
2019-01-12 22:15:36 -0600

    Added Core Files for ReactJS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,10 @@
-
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBOB9TkbAPtIjCrgXC5_vHKVms283aQx4o",
+    authDomain: "project-1-music-trivia.firebaseapp.com",
+    databaseURL: "https://project-1-music-trivia.firebaseio.com",
+    projectId: "project-1-music-trivia",
+    storageBucket: "project-1-music-trivia.appspot.com",
+    messagingSenderId: "743814852234"
+};
+firebase.initializeApp(config);
