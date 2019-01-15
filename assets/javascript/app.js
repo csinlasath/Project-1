fd20cace7f95b2c53723ce5a4bdb201bd6b637c9
Christopher Sinlasath <csinlasath@live.com>
2019-01-14 20:05:20 -0600

    Connected to OMDB and THEMOVEDB API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,47 @@ var config = {
     messagingSenderId: "743814852234"
 };
 firebase.initializeApp(config);
+
+$(document).ready(function() {
+    
+    $("#submit-button").on("click", function(event) {
+        event.preventDefault();
+        var searchType = $("#search-type-selector").val();
+
+        var searchTopic = $("#search-bar").val();
+        var omdbAPIKey = "&apikey=3dc16ac5";
+        
+
+        if (searchType === "Actor/Actress") {
+            var searchPerson = $("#search-bar").val().trim();
+            var theMovieDBAPIKey = "api_key=b3599d7d48ba417da97cd4b6a2911968";
+            var movieLang = "&language=en-US";
+            var personParam = "&query="
+            var queryURL = "https://api.themoviedb.org/3/search/person?" + theMovieDBAPIKey + movieLang + personParam + searchPerson;
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                
+                var searchParam = "/movie_credits?"
+                console.log(response.results[0].id);
+                var queryURL = "https://api.themoviedb.org/3/person/" + response.results[0].id + searchParam + theMovieDBAPIKey + movieLang;
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(responseChild) {
+                    console.log(responseChild);
+                });
+            });
+        }
+        
+        var queryURL = "https://www.omdbapi.com/?t=" + searchTopic + omdbAPIKey;
+
+        
+
+    });
+
+});
